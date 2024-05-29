d81c56c9e0ac330e09beb8e43e25c9d9cb0c5b44
Thomas Rieutord <thomas.rieutord@gmail.com>
2024-05-29 16:20:57 +0100

    Fix formatting

--- example_of_accessing_and_plotting_data.py
+++ example_of_accessing_and_plotting_data.py
@@ -6,25 +6,25 @@ Example code to show how to access the data and plot it
 """
 
 import os
-import numpy as np
-import xarray as xr
-import matplotlib.pyplot as plt
+
 import cartopy.crs as ccrs
 import cartopy.io.img_tiles as cimgt
-
+import matplotlib.pyplot as plt
+import numpy as np
+import xarray as xr
 from metplotlib import plots
+
 import myfunctions
 
-
 # Open netcdf file
-#-------------
+# -------------
 nc_filename = "data/mera-sample-v1.2010-2011-123h.nc"
 ds = xr.open_dataset(nc_filename)
 print(ds)
 
 
 # Plot orography
-#----------------
+# ----------------
 lcc = myfunctions.cartopy_crs_from_proj4(ds.crs)
 x = ds.eastings.values
 y = ds.northings.values
@@ -34,36 +34,45 @@ fig = plt.figure()
 ax = plt.subplot(projection=ccrs.PlateCarree())
 ax.set_extent([-22, 15, 40, 65])
 ax.coastlines(resolution="50m", color="black", linewidth=0.5)
-c = ax.pcolormesh(x, y, z, transform = lcc, cmap="terrain", shading="nearest")
-plt.colorbar(c, shrink=0.5, label = "m asl")
+c = ax.pcolormesh(x, y, z, transform=lcc, cmap="terrain", shading="nearest")
+plt.colorbar(c, shrink=0.5, label="m asl")
 ax.set_title("Orography in MERA")
 fig.show()
 
 
 # Plot spatial fields at a random date
-#--------------------------------------
+# --------------------------------------
 t_idx = 67
 t = ds.valtimes.values[t_idx]
 mslp = ds.air_pressure_at_sea_level.values[t_idx, ::] / 100
 t2m = ds.air_temperature_at_2_metres.values[t_idx, ::] - 273.15
 
-fig, ax = plots.twovar_plot(mslp, t2m, lons=x, lats=y, cl_varfamily = "temp", title = str(t).split(":")[0], figcrs=lcc, datcrs=lcc)
+fig, ax = plots.twovar_plot(
+    mslp,
+    t2m,
+    lons=x,
+    lats=y,
+    cl_varfamily="temp",
+    title=str(t).split(":")[0],
+    figcrs=lcc,
+    datcrs=lcc,
+)
 fig.show()
 
 
 # Plot the time series of average temperatures
-#----------------------------------------------
-avg_t2m = ds.air_temperature_at_2_metres.values.mean(axis = (1,2)) - 273.15
-avg_t30m = ds.air_temperature_at_30_metres.values.mean(axis = (1,2)) - 273.15
-avg_t850 = ds.air_temperature_at_850_hPa.values.mean(axis = (1,2)) - 273.15
-avg_t500 = ds.air_temperature_at_500_hPa.values.mean(axis = (1,2)) - 273.15
+# ----------------------------------------------
+avg_t2m = ds.air_temperature_at_2_metres.values.mean(axis=(1, 2)) - 273.15
+avg_t30m = ds.air_temperature_at_30_metres.values.mean(axis=(1, 2)) - 273.15
+avg_t850 = ds.air_temperature_at_850_hPa.values.mean(axis=(1, 2)) - 273.15
+avg_t500 = ds.air_temperature_at_500_hPa.values.mean(axis=(1, 2)) - 273.15
 t = ds.valtimes.values
 
 fig, ax = plt.subplots()
-ax.plot(t, avg_t2m, label = "T 2m")
-ax.plot(t, avg_t30m, label = "T 30m")
-ax.plot(t, avg_t850, label = "T 850")
-ax.plot(t, avg_t500, label = "T 500")
+ax.plot(t, avg_t2m, label="T 2m")
+ax.plot(t, avg_t30m, label="T 30m")
+ax.plot(t, avg_t850, label="T 850")
+ax.plot(t, avg_t500, label="T 500")
 ax.grid()
 ax.legend()
 ax.set_title("Time series of average temperatures")
--- myfunctions.py
+++ myfunctions.py
@@ -18,20 +18,20 @@ import cartopy.crs as ccrs
 
 def lineparser(line, startword, stopword=None):
     """Extract content from a line of text.
-    
+
     Parameters
     ----------
     line: str
         Line of text from which we extract content
-    
+
     startword: str
         Text pattern marking the begining of the content to extract
-    
+
     stopword: str, optional
         Text pattern marking the end of the content to extract. If not
         provided, the function extracts up to the end of the line.
-    
-    
+
+
     Example
     -------
     >>> line = "This is a line from a log file. Recorded value=0.25645"
@@ -46,14 +46,15 @@ def lineparser(line, startword, stopword=None):
         endidx = startidx + line[startidx:].index(stopword)
     else:
         endidx = len(line) + 1
-    
+
     return line[startidx:endidx]
+
 
 def cartopy_crs_from_proj4(proj4):
     """Return the Cartopy Lambert conformal conic projection corresponding
     to the given string in Proj4 convention.
-    
-    
+
+
     Example
     -------
     >>> proj4 = '+proj=lcc +lat_0=53.5 +lon_0=5.0 +lat_1=53.5 +lat_2=53.5 +x_0=0.0 +y_0=0.0 +datum=WGS84 +units=m +no_defs'
@@ -63,7 +64,7 @@ def cartopy_crs_from_proj4(proj4):
     lon_0 = float(lineparser(proj4, "+lon_0=", " "))
     lat_1 = float(lineparser(proj4, "+lat_1=", " "))
     lat_2 = float(lineparser(proj4, "+lat_2=", " "))
-    
+
     return ccrs.LambertConformal(
         central_longitude=lon_0,
         central_latitude=lat_0,
